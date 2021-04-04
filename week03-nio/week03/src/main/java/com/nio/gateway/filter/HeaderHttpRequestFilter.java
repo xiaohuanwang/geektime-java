680fddc86936443086d2db1931bf4edfc16c26c7
王小欢 <wxh_work@163.com>
2021-04-04 23:20:13 +0800

    commit--filter

--- week03-nio/week03/src/main/java/com/nio/gateway/filter/HeaderHttpRequestFilter.java
+++ week03-nio/week03/src/main/java/com/nio/gateway/filter/HeaderHttpRequestFilter.java
@@ -1,12 +1,30 @@
 package com.nio.gateway.filter;
 
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+
+import java.io.UnsupportedEncodingException;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HeaderHttpRequestFilter implements HttpRequestFilter {
 
     @Override
     public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
         fullRequest.headers().set("mao", "soul");
+        FullHttpResponse response = null;
+        String value = "hello";
+        try {
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
     }
 }
